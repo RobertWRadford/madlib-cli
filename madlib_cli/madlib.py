cce3a69718e2a7c25246948f858b45c87d1f535b
Robert Radford <RobertR928@gmail.com>
2020-11-11 23:47:04 -0500

    final

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,6 +1,9 @@
 import re
+import os
 
 fillPattern = re.compile(r'{.*?}')
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, '../assets/madlib_complete.txt')
 
 print('Greetings! Let\'s have some fun with MadLibs. I will ask you for some words by grammatical family and then place them into a prebuilt story to find some funny results.')
 
@@ -21,10 +24,10 @@ def merge(template, filledIn):
 		template = template.replace(fillIns[i], filledIn[i], 1)
 	return template
 
-template = read_template('../assets/madlib_template.txt')
+template = read_template(filename)
 fillIns = parse(template)
 userInput = newWords(fillIns)
-finished = open('../assets/madlib_complete.txt', 'wt')
+finished = open(filename, 'wt')
 finished.write(merge(template, userInput))
 finished.close()
 print(template)
--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,12 +1,16 @@
 from madlib_cli.madlib import read_template, parse, newWords, merge
+import os
+
+dirname = os.path.dirname(__file__)
+testName = os.path.join(dirname, '../assets/test.txt')
 
 def test_read_template():
-	actual = read_template('assets/test.txt')
+	actual = read_template(testName)
 	expect = 'this is a {test}'
 	assert actual == expect
 
 def test_parse():
-	actual = parse(read_template('assets/test.txt'))
+	actual = parse(read_template(testName))
 	expect = ['{test}']
 	assert actual == expect
 
