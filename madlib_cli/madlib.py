411abf71dc7cc64e26728a57ff44c818bea3d4f2
Robert Radford <RobertR928@gmail.com>
2020-11-12 20:59:38 -0500

    moved production code out of madlib.py

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -3,9 +3,8 @@ import os
 
 fillPattern = re.compile(r'{.*?}')
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, '../assets/madlib_complete.txt')
-
-print('Greetings! Let\'s have some fun with MadLibs. I will ask you for some words by grammatical family and then place them into a prebuilt story to find some funny results.')
+writeFile = os.path.join(dirname, '../assets/madlib_complete.txt')
+readFile = os.path.join(dirname, '../assets/madlib_template.txt')
 
 def read_template(path):
 	return open(path, 'rt').read()
@@ -23,11 +22,3 @@ def merge(template, filledIn):
 	for i in range(len(filledIn)):
 		template = template.replace(fillIns[i], filledIn[i], 1)
 	return template
-
-template = read_template(filename)
-fillIns = parse(template)
-userInput = newWords(fillIns)
-finished = open(filename, 'wt')
-finished.write(merge(template, userInput))
-finished.close()
-print(template)
